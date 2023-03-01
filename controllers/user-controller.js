88a0047da9cd8789a77608828154d2025a570f11
Will <willkim0608@gmail.com>
2023-03-01 15:57:16 -0800

    Add getUserById to controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,6 +18,32 @@ const userController = {
         console.log(err);
         res.status(500).json(err);
       });
+  },
+
+  // Get one user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => {
+        // If no user is found, send 404 error
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   }
 };
 
