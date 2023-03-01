96bf1325f226b8ae41decab7689dd32c0b0e5d39
Will <willkim0608@gmail.com>
2023-03-01 15:56:14 -0800

    Add getAllUsers to controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,24 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+  // Get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+};
+
+module.exports = userController;
