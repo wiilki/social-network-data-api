9bdbad98dabdb5545bab980d81b3689acaa1113e
Will <willkim0608@gmail.com>
2023-03-01 11:56:19 -0800

    Add api routes to server.js

--- server.js
+++ server.js
@@ -2,19 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+// Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
+// Routes
+const userRoutes = require('./routes/api/user-routes');
+const thoughtRoutes = require('./routes/api/thought-routes');
+const reactionRoutes = require('./routes/api/reaction-routes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+app.use('/api/reactions', reactionRoutes);
+
+// Database connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetworkdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
-mongoose.set('debug', true);
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to database');
+});
 
-app.use(require('./routes/api'));
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Server listening
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
